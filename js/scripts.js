b656d1b8381a00177c7e0c5f12b4857b5f98b0a0
Chris Stinson <cistinson@lcmail.lcsc.edu>
2020-02-23 12:29:56 -0800

    add image folder and generate rank function

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,46 @@ $(document).ready(function() {
   var logoutClick = $("#logoutSubmit");
 
   //Declaring div wrapper variables for login and logged in screens
-  var loginWrapper = $("#loginWrapper");
-  var loggedInWrapper = $("#loggedInWrapper");
-  
+  var loginWrapper = $(".loginWrapper");
+  var loggedInWrapper = $(".loggedInWrapper");
+
+  //Obtain username and password from input fields
+  var username;
+  var password;
+  var usernamePlaceholder = $("#usernamePlaceholder");
+
+  //Declaring rank options
+
+  var rank;
+  var rankOptions = ['Bronze 1', 'Bronze 2', 'Bronze 3', 'Silver 1', 'Silver 2', 'Silver 3', 'Gold 1', 'Gold 2', 'Gold 3', 'Platinum 1', 'Platinum 2', 'Platinum 3', 'Diamond 1', 'Diamond 2', 'Diamond 3', 'Champion 1', 'Champion 2', 'Champion 3', 'Grand Champion'];
+  var rankPlaceholder = $("#rankPlaceholder");
+
   //Moves user to logged in screen
-  loginClick.on("click", function(){
-    event.preventDefault();
-    alert("you're logging in");
+  loginClick.click(function(){
+    username = $("#username").val();
+    password = $("#password").val();
+    if ( username !== '' && password !== ''){
+      event.preventDefault();
+      loginWrapper.hide();
+      loggedInWrapper.show();
+      usernamePlaceholder.text(username);   
+    } 
+    generateRank();
   });
 
   //Moves user to logged out screen
-  logoutClick.on("click", function(){
+  logoutClick.click(function(){
     event.preventDefault();
-    alert("you're logging out");
+    loginWrapper.show();
+    loggedInWrapper.hide();    
+    $("#username").val('');
+    $("#password").val('');
   });
+
+  //Generates random number for rank array
+  function generateRank(){
+    rank = Math.floor(Math.random() * 19);
+    rankPlaceholder.text(rankOptions[rank]);
+  }
 
 });
